15b498f58d537081cde57378dfab5e5ac232729d
Willian-Cueva <enriquewillian2@gmail.com>
2022-08-12 00:20:48 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/aiothings/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aiothings
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:aiothings/pages/conection_sp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ConectionSP(title: 'Conection with SP32'),
+    );
+  }
+}
